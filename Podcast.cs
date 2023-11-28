31199d18685fed86639acbcd5f8f5471f167b65e
Lucas Pinheiro <lucaslogos@gmail.com>
2023-11-28 17:13:22 -0300

    update

--- Podcast.cs
+++ Podcast.cs
@@ -1,32 +1,36 @@
 class Podcast
 {
-    private List<Episodio> episodios = new List<Episodio>();
+    private List<Episodio> episodios = new();
     public Podcast(string nome, string? host)
     {
         Nome = nome;
         Host = host;
 
     }
-    public int TotalEpisodios => episodios.Count();
+    public int TotalEpisodios => episodios.Count;
     public string Nome { get; }
     public string? Host { get; }
-    public Episodio? Episodio { get; set; }
-    public Convidado? Convidado { get; set; }
+
     public void ExibeDetalhesPodcast()
     {
-        Console.WriteLine($"Olá você está no {Nome} eu sou o {Host} e nós temos o total de {TotalEpisodios} episodios");
+        Console.WriteLine($"\nOlá você está no {Nome} eu sou o {Host} e nós temos o total de {TotalEpisodios} episodios\n\n");
+
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
     }
     public void AdicionaEpisodios(Episodio episodio)
     {
         episodios.Add(episodio);
     }
-    public void ExibeEpisodios()
-    {
-        Console.WriteLine($"Lista de episodios do podcast: {Nome}");
-        foreach (var episodio in episodios)
-        {
-            Console.WriteLine($"Episodio: {episodio.Nome}");
+    // public void ExibeEpisodios()
+    // {
+    //     Console.WriteLine($"Lista de episodios do podcast: {Nome}");
+    //     foreach (var episodio in episodios)
+    //     {
+    //         Console.WriteLine($"Episodio: {episodio.Nome} ");
 
-        }
-    }
+    //     }
+    // }
 }
